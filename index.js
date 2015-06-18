050b0792cab8f29f551f28c96a4c4a6153e3723e
Daniel Andefors <daniel.andefors@gmail.com>
2015-06-18 14:17:25 -0700

    some improvements

--- index.js
+++ index.js
@@ -5,11 +5,12 @@
 var enovia = require('./lib/enovia-xml-http.js');
 var readline = require('readline');
 var colors = require('colors');
+var url = require('url');
 
 var rl = readline.createInterface(process.stdin, process.stdout, completer);
 
 function completer(line) {
-  var completions = 'connect exit quit'.split(' ')
+  var completions = 'login url quit'.split(' ')
   var hits = completions.filter(function(c) {
     if (c.indexOf(line) == 0) {
       return c;
@@ -18,14 +19,94 @@ function completer(line) {
   return [hits && hits.length ? hits : completions, line];
 }
 
+/** compute the prompt */
 function prompt() {
-  var user = enovia.user;
-  if (user) {
-    rl.setPrompt("[".cyan + user.magenta + "] ".cyan + "$ ", user.length + 5);
-  } else {
-    rl.setPrompt("$ ", 2);
+
+  var pt = '';
+  var ptlen = 0;
+
+  // add server info
+  var server = enovia.server;
+  if (server) {
+
+    pt += '~ '.cyan;
+    ptlen += 2;
+
+    // add user info
+    var user = enovia.user;
+    if (user) {
+      pt += user.yellow + " @ ".cyan;
+      ptlen += user.length + 3;
+    }
+
+    pt += server.magenta;
+    ptlen += server.length;
   }
+
+  pt += " $ ".cyan;
+  ptlen += 2;
+
+  rl.setPrompt(pt, ptlen);
   rl.prompt();
+
+}
+
+/** Asks the user a question */
+function ask(name, defaultValue, callback) {
+  var question = "? ".green;
+  question += name + ": ";
+  if (defaultValue) {
+    question += ("[" + defaultValue + "] ").grey;
+  }
+  rl.question(question, function(newValue) {
+    callback(newValue || defaultValue || '');
+  });
+}
+
+function askUri(callback) {
+  ask('url', enovia.uri, function(x) {
+    if (!x.match(/^https?:\/\/.*$/)) {
+      x = 'http://' + x;
+    }
+    var parsed = url.parse(x);
+    enovia.uri = parsed.href;
+    callback();
+  });
+}
+
+function ensureUri(callback) {
+  enovia.uri ? callback() : askUri(callback);
+}
+
+function askCredentials(callback) {
+  ask('user', enovia.user || 'creator', function(user) {
+    ask('password', '', function(passwd) {
+      callback(user, passwd);
+    });
+  });
+}
+
+function startSpinner(message) {
+  var stream = process.stderr;
+  if (!stream.isTTY) {
+    return function() {};
+  }
+  var tick = 0;
+  var symbols = "-\\|/";
+  var preamble = message || '';
+  var interval = setInterval(function() {
+    stream.cursorTo(0);
+    stream.write((preamble + symbols[tick % symbols.length]).cyan + " ");
+    stream.clearLine(1);
+    tick++;
+  }, 350);
+  return {
+    cancel: function() {
+      clearInterval(interval);
+      stream.cursorTo(0);
+      stream.clearLine(1);
+    }
+  };
 }
 
 rl.on('line', function(line) {
@@ -35,19 +116,28 @@ rl.on('line', function(line) {
     case "exit":
       rl.close();
       break;
-    case "connect":
-      rl.question("? User: ".blue + "[creator] ".yellow.dim, function(user) {
-        rl.question("? Password: ".blue, function(password) {
+    case "uri":
+    case "url":
+    case "server":
+      askUri(function() {
+        prompt();
+      });
+      break;
+    case "login":
+      ensureUri(function() {
+        askCredentials(function(user, password) {
           rl.pause();
-          setTimeout(function() {
-            enovia.connect({user: user, password: password}, function(error, user) {
-              if (error) {
-                console.log(error.toString().red);
-              }
-              rl.resume();
-              prompt();
-            });
-          }, 10);
+          var spinner = startSpinner();
+          enovia.login({user: user, password: password}, function(error, user) {
+            spinner.cancel();
+            if (error) {
+              console.log(error.toString().red);
+            } else {
+              console.log("Success!".green);
+            }
+            rl.resume();
+            prompt();
+          });
         });
       });
       break;
