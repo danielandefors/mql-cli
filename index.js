6c6abcce2b4482699c4e3d522681a983c9850fee
Daniel Andefors <daniel.andefors@gmail.com>
2015-06-18 18:08:54 -0700

    monitor server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var url = require('url');
 var rl = readline.createInterface(process.stdin, process.stdout, completer);
 
 function completer(line) {
-  var completions = 'login url quit'.split(' ')
+  var completions = 'login url info monitor quit'.split(' ')
   var hits = completions.filter(function(c) {
     if (c.indexOf(line) == 0) {
       return c;
@@ -140,12 +140,67 @@ function eq(keyword, minlen, s) {
   return s.length >= minlen && keyword.indexOf(s.toLowerCase()) === 0;
 }
 
+function printData(data, columns) {
+  columns = columns || 20;
+  for (var key in data) {
+    var value = data[key];
+    var line = '  ' + key;
+    while (line.length < columns) {
+      line += ' ';
+    }
+    line += " : ".grey + value;
+    console.log(line);
+  }
+}
+
+function forEach(maybeArray, callback) {
+  Array.isArray(maybeArray) ?
+    maybeArray.forEach(callback) :
+    [maybeArray].forEach(callback)
+}
+
+function singleValue(maybeValue) {
+  return Array.isArray(maybeValue) ? maybeValue[0] : maybeValue;
+}
+
+function prettyDuration(durStr) {
+  var matches = durStr.match(/^P(\d+H)?(\d+M)?(\d+S)$/);
+  if (!matches) return durStr;
+  var pretty = [];
+  for (var i = 1; i < matches.length; i++) {
+    var x = matches[i];
+    if (!x) continue;
+    var value = parseInt(x);
+    var unit = x.charAt(x.length-1);
+    switch (unit) {
+      case 'H': unit = 'hour'; break;
+      case 'M': unit = 'minute'; break;
+      case 'S': unit = 'second'; break;
+    }
+    if (value !== 1) {
+      unit += 's';
+    }
+    pretty.push(value + ' ' + unit);
+  }
+  return pretty.join(', ');
+}
+
 rl.on('line', function(line) {
   switch (line.trim()) {
     case "q":
     case "quit":
     case "exit":
       rl.close();
+      break;
+    case "info":
+      console.log('');
+      printData({
+        'url': enovia.uri,
+        'user': enovia.user,
+        'session-id': enovia.sessionId
+      }, 12);
+      console.log('');
+      prompt();
       break;
     case "uri":
     case "url":
@@ -161,6 +216,67 @@ rl.on('line', function(line) {
         login(user, password, function() {
           prompt();
         });
+      });
+      break;
+    case "monitor":
+      // Get monitor data from server
+      rl.pause();
+      var spinner = startSpinner();
+      enovia.monitorServer(function(error, data) {
+        spinner.cancel();
+        if (error) {
+          console.log(error.toString().red);
+        } else if (data) {
+
+          // Pretty print server information
+
+          console.log('');
+
+          var header = data.monitorServer.header;
+          printData({
+            'server-product': header.product,
+            'server-version': header.version,
+            'server-uptime': prettyDuration(header.active),
+            'server-debug-port': header.port,
+            'server-pid': header.pid
+          });
+
+          forEach(data.monitorServer.threadDump.thread, function(thread, index) {
+
+            // Skip our own threads...
+            if (thread.sessionId === enovia.sessionId) {
+              return;
+            }
+
+            // Pretty print thread and stack information...
+            console.log('\n  thread @ ' + thread.id + ": ");
+            forEach(thread.stackTrace.stackEntry, function(entry) {
+              var depth = entry.depth;
+              var data = {};
+              var duration = '';
+              if (entry.mqlTraceEntry) {
+                duration = prettyDuration(singleValue(entry.mqlTraceEntry.active));
+                data['  mql-command'] = entry.mqlTraceEntry.command;
+              } else if (entry.adkTraceEntry) {
+                duration = prettyDuration(singleValue(entry.adkTraceEntry.active));
+                data['  adk-method'] = entry.adkTraceEntry.method;
+              } else if (entry.backgroundTaskTrace) {
+                duration = prettyDuration(singleValue(entry.backgroundTaskTrace.active));
+                data['  bg-task-type'] = entry.backgroundTaskTrace.taskType;
+                data['  bg-task-parent'] = entry.backgroundTaskTrace.parentThreadId;
+              } else {
+                console.log(entry);
+              }
+              console.log('\n    stack #' + depth + ": " + ("[" + duration + "]").grey);
+              printData(data);
+            });
+          });
+
+          console.log('');
+
+        }
+        rl.resume();
+        prompt();
       });
       break;
     default:
